3c8aa060bb45e48c0e0fcb7abe131e16433725ca
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2025-02-12 22:02:52 +0100

    Support multiple configurations transforming resources

--- src/main/java/net/lenni0451/repackager/RepackagerPlugin.java
+++ src/main/java/net/lenni0451/repackager/RepackagerPlugin.java
@@ -7,17 +7,16 @@ import net.lenni0451.repackager.transforms.RepackageTransform;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.artifacts.ModuleDependency;
 import org.gradle.api.artifacts.type.ArtifactTypeDefinition;
 
 public class RepackagerPlugin implements Plugin<Project> {
 
-    public static final String REPACKAGED_JAR_TYPE = "repackaged-jar";
-
     @Override
     public void apply(Project target) {
-        RepackagerExtension repackagerExtension = target.getExtensions().create("repackager", RepackagerExtension.class);
+        RepackagerExtension repackagerExtension = target.getExtensions().create("repackager", RepackagerExtension.class, target);
         DependencyRepackagerExtension dependencyRepackagerExtension = target.getExtensions().create("dependencyRepackager", DependencyRepackagerExtension.class);
 
         target.afterEvaluate(project -> {
@@ -38,10 +37,12 @@ public class RepackagerPlugin implements Plugin<Project> {
         });
         target.afterEvaluate(project -> {
             if (dependencyRepackagerExtension.getConfiguration().isPresent()) {
-                for (Dependency dependency : dependencyRepackagerExtension.getConfiguration().get().getAllDependencies()) {
+                Configuration configuration = dependencyRepackagerExtension.getConfiguration().get();
+                String jarType = "repackaged-jar-" + configuration.getName();
+                for (Dependency dependency : configuration.getAllDependencies()) {
                     if (!(dependency instanceof ModuleDependency)) continue;
                     ModuleDependency moduleDependency = (ModuleDependency) dependency;
-                    moduleDependency.attributes(attributeContainer -> attributeContainer.attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, RepackagerPlugin.REPACKAGED_JAR_TYPE));
+                    moduleDependency.attributes(attributeContainer -> attributeContainer.attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, jarType));
                 }
                 target.getDependencies().registerTransform(RepackageTransform.class, transform -> {
                     transform.getParameters().getRelocations().set(dependencyRepackagerExtension.getRelocations());
@@ -51,7 +52,7 @@ public class RepackagerPlugin implements Plugin<Project> {
                     transform.getParameters().getRemoveEmptyDirs().set(dependencyRepackagerExtension.getRemoveEmptyDirs());
 
                     transform.getFrom().attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, ArtifactTypeDefinition.JAR_TYPE);
-                    transform.getTo().attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, REPACKAGED_JAR_TYPE);
+                    transform.getTo().attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, jarType);
                 });
             }
         });
--- src/main/java/net/lenni0451/repackager/extensions/RepackagerExtension.java
+++ src/main/java/net/lenni0451/repackager/extensions/RepackagerExtension.java
@@ -1,5 +1,11 @@
 package net.lenni0451.repackager.extensions;
 
+import net.lenni0451.repackager.transforms.RepackageTransform;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.ModuleDependency;
+import org.gradle.api.artifacts.type.ArtifactTypeDefinition;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
@@ -8,9 +14,14 @@ import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputFile;
 
+import java.util.Map;
+
 public abstract class RepackagerExtension {
 
-    public RepackagerExtension() {
+    private final Project project;
+
+    public RepackagerExtension(final Project project) {
+        this.project = project;
         this.getRemapStrings().convention(false);
         this.getRemapServices().convention(true);
         this.getRemapManifest().convention(true);
@@ -38,5 +49,26 @@ public abstract class RepackagerExtension {
 
     @Input
     public abstract Property<Boolean> getRemoveEmptyDirs();
+
+    public void repackageDependencies(final Configuration configuration, final Map<String, String> relocations, final boolean remapStrings, final boolean remapServices, final boolean remapManifest, final boolean removeEmptyDirs) {
+        this.project.afterEvaluate(project -> {
+            String jarType = "repackaged-jar-" + configuration.getName();
+            for (Dependency dependency : configuration.getAllDependencies()) {
+                if (!(dependency instanceof ModuleDependency)) continue;
+                ModuleDependency moduleDependency = (ModuleDependency) dependency;
+                moduleDependency.attributes(attributeContainer -> attributeContainer.attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, jarType));
+            }
+            project.getDependencies().registerTransform(RepackageTransform.class, transform -> {
+                transform.getParameters().getRelocations().set(relocations);
+                transform.getParameters().getRemapStrings().set(remapStrings);
+                transform.getParameters().getRemapServices().set(remapServices);
+                transform.getParameters().getRemapManifest().set(remapManifest);
+                transform.getParameters().getRemoveEmptyDirs().set(removeEmptyDirs);
+
+                transform.getFrom().attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, ArtifactTypeDefinition.JAR_TYPE);
+                transform.getTo().attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, jarType);
+            });
+        });
+    }
 
 }
